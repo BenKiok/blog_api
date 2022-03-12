9ffad8c221fdf7e71ae0d81eaeac1d308f60d6c9
Benedict Kioko <benedictmkioko@gmail.com>
2022-03-12 10:34:21 -0500

    Outline comment controller functions

---
+++ controllers/commentController.js
@@ -0,0 +1,17 @@
+const Comment = require('../models/Comment');
+
+exports.view_comments_get = (req, res, next) => {
+  res.json('View comments get route undefined');
+}
+
+exports.create_comment_get = (req, res, next) => {
+  res.json('Create comment get route undefined');
+}
+
+exports.create_comment_post = (req, res, next) => {
+  res.json('Create comment post route undefined');
+}
+
+exports.delete_comment_post = (req, res, next) => {
+  res.json('Delete comment post route undefined');
+}
