2c1b9d72b5ac74c59b9034ff302822ffddd2392b
Benedict Kioko <benedictmkioko@gmail.com>
2022-03-12 11:05:24 -0500

    Change comment controller names to match REST specifications

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,14 +4,10 @@ exports.view_comments_get = (req, res, next) => {
   res.json('View comments get route undefined');
 }
 
-exports.create_comment_get = (req, res, next) => {
-  res.json('Create comment get route undefined');
-}
-
 exports.create_comment_post = (req, res, next) => {
   res.json('Create comment post route undefined');
 }
 
-exports.delete_comment_post = (req, res, next) => {
+exports.delete_comment_delete = (req, res, next) => {
   res.json('Delete comment post route undefined');
 }
