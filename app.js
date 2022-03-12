83645dbde23357468c082f9dd745e6359c76b7db
Benedict Kioko <benedictmkioko@gmail.com>
2022-03-12 09:34:02 -0500

    Configure main route with relevant controller

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const indexRouter = require('./routes/index');
+
 const app = express();
 
 const mongodb = process.env.MONGODB;
@@ -9,8 +11,6 @@ mongoose.connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongo connection error'));
 
-app.get('/', (req, res) => {
-  res.send('App is functional!');
-})
+app.use('/', indexRouter);
 
 app.listen(process.env.PORT, () => console.log('App listening on port ' + process.env.PORT));
