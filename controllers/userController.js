eadbec78822156395c51ec315a4bedf1d1731e64
Benedict Kioko <benedictmkioko@gmail.com>
2022-03-18 15:57:07 -0400

    Fill out user controller

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const User = require('../models/User.js');
+
+exports.login_user_post = (req, res, next) => {
+  res.json('Login user post route not complete');
+}
+
+exports.logout_user_post = (req, res, next) => {
+  res.json('Logout user post route not complete');
+}
